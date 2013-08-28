9c4e7ddaa7e40fad2d0373ca4ea47246841092a3
Dmitry Davydov <haudvd@gmail.com>
2013-08-28 22:09:20 +0400

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HashteshRu::Application.config.secret_token = '5a2343a4a11ff6046a0f57b675bd24d550b920b74016a7a2661702f81e17ed6cf4b456901e453c789095755f5b5f88a0e4e4f38cbb014fa11a8136d132ca6062'
