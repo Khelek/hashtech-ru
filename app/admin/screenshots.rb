72f33425250e1f0a16f28cf1793e9f2dd22b118c
Dmitry Davydov <haudvd@gmail.com>
2013-10-15 17:31:24 +0400

    create screenshots of in the admin panel

--- app/admin/screenshots.rb
+++ app/admin/screenshots.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Screenshot do
-  
+    index do
+    selectable_column
+    column "project name" do |scrn|
+      link_to scrn.project.name, admin_project_path(scrn.project)
+    end
+    column :image do |scrn|
+      image_tag(scrn.image_url(:thumb), alt: scrn.image)
+    end
+    column "image_path" do |scrn|
+      scrn.image
+    end
+    default_actions
+  end
+
 end
